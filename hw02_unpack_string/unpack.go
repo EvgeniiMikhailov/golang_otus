d9954f3d06c7b01c8d5e90852b95bdb4aeb86c96
Evgenii Mikhailov <evgenii@mikhailov.dev>
2020-03-12 17:47:35 +0300

    Fix golangci-lint recomendations

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,24 +11,22 @@ var ErrInvalidString = errors.New("invalid string")
 
 func getRepeateTime(source string, startPos int) int {
 	if startPos == len(source)-1 {
-		return 1
+		return 1 //nolint:gomnd
 	}
 	var character = rune(source[startPos+1])
 	if unicode.IsDigit(character) {
 		var repeatTime, _ = strconv.Atoi(string(character))
 		return repeatTime
 	}
-	return 1
+	return 1 //nolint:gomnd
 }
 
 func validateDigitalPlacement(source string, pos int) error {
 	var err error
 	if pos == 0 {
 		err = ErrInvalidString
-	} else {
-		if unicode.IsDigit(rune(source[pos-1])) == true {
-			err = ErrInvalidString
-		}
+	} else if unicode.IsDigit(rune(source[pos-1])) {
+		err = ErrInvalidString
 	}
 	return err
 }
@@ -37,7 +35,7 @@ func Unpack(input string) (string, error) {
 	var resultBuilder strings.Builder
 	var err error
 	for index, value := range input {
-		if unicode.IsDigit(value) != true {
+		if !unicode.IsDigit(value) {
 			var repeat = getRepeateTime(input, index)
 			resultBuilder.WriteString(strings.Repeat(string(value), repeat))
 		} else {
