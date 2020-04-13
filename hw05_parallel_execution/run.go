0cb8e7de1d1e1f0e243d59bf5aea6a4275a81246
Evgenii Mikhailov <evgenii@mikhailov.dev>
2020-04-13 19:47:16 +0300

    implement queue without error handling

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,26 @@ type Task func() error
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
-	// Place your code here
+	wg := &sync.WaitGroup{}
+	tasksCh := make(chan Task)
+
+	for i := 0; i < N; i++ {
+		go startWorker(wg, tasksCh)
+	}
+
+	for _, task := range tasks {
+		wg.Add(1)
+		tasksCh <- task
+	}
+
+	wg.Wait()
+	close(tasksCh)
 	return nil
+}
+
+func startWorker(wg *sync.WaitGroup, tasks <-chan Task) {
+	for task := range tasks {
+		task()
+		wg.Done()
+	}
 }
