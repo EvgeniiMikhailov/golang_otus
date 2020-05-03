38372a0b4d88a96f98c037563f981a119b3731ab
Evgenii Mikhailov <evgenii@mikhailov.dev>
2020-05-03 14:12:23 +0300

    complete copy part

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"io"
 	"os"
 )
 
@@ -10,32 +11,40 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func openSourceFile(fromPath string, offset int64) (*os.File, error) {
+func openSourceFile(fromPath string, offset, limit int64) (*os.File, int64, error) {
+	readLimit := limit
+
 	source, err := os.Open(fromPath)
 	if err != nil {
-		return nil, err
+		return nil, readLimit, err
 	}
 	sourceFI, err := source.Stat()
 	if err != nil {
-		return nil, err
+		return nil, readLimit, err
 	}
 
 	if !sourceFI.Mode().IsRegular() {
-		return nil, ErrUnsupportedFile
+		return nil, readLimit, ErrUnsupportedFile
 	}
 
-	if offset > sourceFI.Size() {
-		return nil, ErrOffsetExceedsFileSize
+	fileSize := sourceFI.Size()
+	if offset > fileSize {
+		return nil, readLimit, ErrOffsetExceedsFileSize
 	}
 
 	if offset != 0 {
 		source.Seek(offset, 0)
 	}
-	return source, nil
+
+	if limit == 0 || limit > fileSize-offset {
+		readLimit = fileSize - offset
+	}
+
+	return source, readLimit, nil
 }
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	source, err := openSourceFile(fromPath, offset)
+	source, limit, err := openSourceFile(fromPath, offset, limit)
 	defer source.Close()
 	if err != nil {
 		return err
@@ -43,6 +52,14 @@ func Copy(fromPath string, toPath string, offset, limit int64) error {
 
 	destination, err := os.Create(toPath)
 	defer destination.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = io.CopyN(destination, source, limit)
+	if err == io.EOF {
+		return nil
+	}
 	if err != nil {
 		return err
 	}
