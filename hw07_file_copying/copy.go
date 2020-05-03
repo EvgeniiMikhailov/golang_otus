117e18f548c2ec348d69059e147d7369e1eec536
Evgenii Mikhailov <evgenii@mikhailov.dev>
2020-05-03 13:33:25 +0300

    add open source file function

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"os"
 )
 
 var (
@@ -9,7 +10,42 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+func openSourceFile(fromPath string, offset int64) (*os.File, error) {
+	source, err := os.Open(fromPath)
+	if err != nil {
+		return nil, err
+	}
+	sourceFI, err := source.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if !sourceFI.Mode().IsRegular() {
+		return nil, ErrUnsupportedFile
+	}
+
+	if offset > sourceFI.Size() {
+		return nil, ErrOffsetExceedsFileSize
+	}
+
+	if offset != 0 {
+		source.Seek(offset, 0)
+	}
+	return source, nil
+}
+
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	source, err := openSourceFile(fromPath, offset)
+	defer source.Close()
+	if err != nil {
+		return err
+	}
+
+	destination, err := os.Create(toPath)
+	defer destination.Close()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
